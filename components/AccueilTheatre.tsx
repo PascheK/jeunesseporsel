2a86ec09c252c39c51ccda98fc31e1d56e635c84
PascheK <killian.pasche7@gmail.com>
2024-12-23 06:15:16 +0100

    Update AccueilTheatre component styles and adjust image responsiveness

--- components/AccueilTheatre.tsx
+++ components/AccueilTheatre.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const AccueilTheatre = () => {
   return (
-    <section className='size-full flex items-center justify-center bg-jeunesse text-jeunesse-white ' id='accueil-theatre'>
+    <section className='w-full h-[130%] sm:h-full flex items-center justify-center bg-jeunesse text-jeunesse-white ' id='accueil-theatre'>
       <div className='flex flex-row flex-wrap justify-center sm:w-4/5 gap-20'>
       <div className='flex flex-col items-center justify-center gap-4  text-center lg:w-1/2'>
         <h1 className='h1'>Soucis a l&apos;étude</h1>
@@ -15,7 +15,9 @@ const AccueilTheatre = () => {
         </Link>
       </div>
       <div className=''>
-        <Image src='/assets/images/theatre_affiche.jpg' alt='affiche théatre' width={'350'} height={'150'}/>
+        <Image className='sm:block hidden' src='/assets/images/theatre_affiche.jpg' alt='affiche théatre' width={'350'} height={'150'}/>
+        <Image className='sm:hidden block' src='/assets/images/theatre_affiche.jpg' alt='affiche théatre' width={'250'} height={'150'}/>
+
       </div>
       </div>
       
