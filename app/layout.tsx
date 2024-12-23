2dab8d2ab76e25572ef6cde8384191fd27f9baaa
PascheK <killian.pasche7@gmail.com>
2024-12-23 05:47:29 +0100

    Add Vercel Analytics integration and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <Analytics/>
       <body className={`${poppins.variable} font-poppins antialiased`} >
         {children}
       </body>
